44c19ed4088361ab438bb1cb5be1614763284f34
dk-786 <167399983+dk-786@users.noreply.github.com>
2025-10-07 16:31:11 +0530

    end changes

--- src/helpers/timezoneTransform.js
+++ src/helpers/timezoneTransform.js
@@ -10,12 +10,12 @@ export const timezoneTransform = function (doc, ret) {
   if (ret.createdAt) {
     ret.createdAt = moment(ret.createdAt)
       .tz("Asia/Kolkata")
-      .format("HH:mm:ss A | DD-MM-YYYY ");
+      .format("YYYY-MM-DD HH:mm:ss");
   }
   if (ret.updatedAt) {
     ret.updatedAt = moment(ret.updatedAt)
       .tz("Asia/Kolkata")
-      .format("HH:mm:ss A | DD-MM-YYYY");
+      .format("YYYY-MM-DD HH:mm:ss");
   }
   return ret;
 };
